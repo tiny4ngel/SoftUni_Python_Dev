209a49cd2c4120d85fa27a4e0cae0ee931e8d2d2
Julie Ivanova <66208546+tiny4ngel@users.noreply.github.com>
2022-11-02 23:27:15 +0200

    del

--- shop/product_repository.py
+++ shop/product_repository.py
@@ -26,12 +26,3 @@ class ProductRepository:
         return '\n'.join([f"{product}: {product.quantity}" for product in self.products])
 
 
-food = Food("apple")
-drink = Drink("water")
-repo = ProductRepository()
-repo.add(food)
-repo.add(drink)
-print(repo.products)
-print(repo.find("water"))
-repo.find("apple").decrease(5)
-print(repo)
